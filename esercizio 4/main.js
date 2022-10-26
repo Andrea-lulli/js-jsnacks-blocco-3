3e18b35a564862d59155414ffe758cde9538e4a6
Andrea-Lulli <andrea.lulli89@gmail.com>
2022-10-26 16:33:34 +0200

    esercizio 4 completo

--- esercizio 4/main.js
+++ esercizio 4/main.js
@@ -4,3 +4,31 @@
 // suo nome e comunicagli se può partecipare o no alla 
 // festa.
 // Interrompi il ciclo appena il nome è stato trovato.
+
+
+
+let nomeInvitati = ["andrea", "luca", "mirko", "gigi", "alberto"];
+
+
+
+let listaInvitati = document.getElementById("stampa")
+
+let richiesta = prompt("inserisci nome")
+
+
+for (let i = 0; i < nomeInvitati.length; i++) {
+
+
+
+    if (richiesta == nomeInvitati[i]) {
+
+        stampa.innerHTML = "nome in lista";
+        break;
+
+    } else if (richiesta != nomeInvitati[i]) {
+        stampa.innerHTML = "non puo partecipare alla festa";
+
+    }
+
+}
+
