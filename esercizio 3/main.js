c86c7e3e49ad03d6ae76a7f3c9a99cb877254cc9
Andrea-Lulli <andrea.lulli89@gmail.com>
2022-10-26 17:10:59 +0200

    esercizio 3 completo

--- esercizio 3/main.js
+++ esercizio 3/main.js
@@ -5,5 +5,18 @@
 // ognuno formato da 10 numeri casuali da 1 a 100.
 // Ogni volta che ne crei uno, stampalo.
 
+let stampa = document.getElementById("stampa")
 
+let numero = parseInt(prompt("inserisci numero"))
 
+for (let i = 0; i < numero; i++) {
+
+    let array = [i]
+
+    for (let i = 0; i < 10; i++) {
+        array = Math.floor(Math.random() * 10)
+        
+        stampa.innerHTML += array
+        
+    }
+}
